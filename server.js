3afc4b4465e922c9f9b02a3922f6f4b088ce4842
Benjamin-del <ben@benja.ml>
2021-11-25 19:03:51 -0500

    Create server.js

--- server.js
+++ server.js
@@ -5,23 +5,13 @@ const client = new Discord.Client();
 const prefix = "!"; // just an example, change to whatever you want
 const { embeds } = require("discord.js");
 
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-const data = new SlashCommandBuilder()
-	.setName('setname')
-	.setDescription('setdiscription')
-	.addStringOption(option =>
-		option.setName('op.setname')
-			.setDescription('op.discription')
-			.setRequired(true));
-
 client.on("message", message => {
   if (!message.content.startsWith(prefix)) return;
 
   const args = message.content.trim().split(/ +/g);
   const cmd = args[0].slice(prefix.length).toLowerCase(); // case INsensitive, without prefix
 
-  if (cmd === "ping") message.reply("pong");
+  if (cmd === "ping") message.reply("I am still here!");
 
   if (cmd === "stop" || cmd === "stp" || cmd === "s") {
     if (args[1] != null) {
@@ -33,7 +23,12 @@ client.on("message", message => {
         .setDescription("Results for the next busses at stop #" + args[1])
         .setThumbnail("https://map.oc-bot.tk/img/oc-bot-finial.png")
       message.channel.send({ embed });
-    } 
+    }
+  if (cmd === "help") {
+ 	message.channel.send("Hello! I am glad to help you!")
+	message.channel.send("I am a bot that provides near real-time data on Busses in the City Of Ottawa!")
+	message.channel.send("Stop numbers: I re
+    }
   }
 });
 client.login(process.env.BOT_TOKEN);
